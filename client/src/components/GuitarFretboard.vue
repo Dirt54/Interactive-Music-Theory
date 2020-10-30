be7f9ab42d166db6fc862b6686c39a5a1ac3b37c
Luke Robinson <lukerobinsona@gmail.com>
2020-10-30 15:39:14 -0400

    Setup and Structure

---
+++ client/src/components/GuitarFretboard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Hey there</h1>
+    <ul id="v-for-object" class="demo">
+      <li v-for="value in notesArray" :key="value">
+        {{ value.note }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      notesArray : [{
+        number: 1,
+        note: "a"
+      },
+        {
+        number: 2,
+        note: "b"
+      },
+      ],
+    };
+  },
+}
+</script>
